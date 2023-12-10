3d199eda65ae2de8e99af78af0408516e845a5ce
Bangoura <53382602+AlphaOumar10@users.noreply.github.com>
2023-12-10 17:13:54 +0100

    first commit

---
+++ menu.php
@@ -0,0 +1,24 @@
+<!-- ======= Header ======= -->
+<header id="header" class="header d-flex align-items-center">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+      <a href="index.php" class="logo d-flex align-items-center">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <h1>Constuction<span>.</span></h1>
+      </a>
+
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="index.php" class="active">Accueil</a></li>
+          <li><a href="about.php">A propos de nous</a></li>
+          <li><a href="services.php">Services</a></li>
+          <li><a href="projects.php">Projets</a></li>
+          <li><a href="contact.php">Contact</a></li>
+        </ul>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
